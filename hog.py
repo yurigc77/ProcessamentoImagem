488ef8f7ca4f7f3c332d482f15a55760c35cef2b
Yuri Garcia Campos <yurigc@yahoo.com.br>
2022-12-06 19:04:17 -0300

    correção

--- hog.py
+++ hog.py
@@ -15,31 +15,30 @@ image=pp.filtro("px",image)
 image2=pp.filtro("px",image2)
 
 #variaveis de hog
-winSize = (64,64)
-blockSize = (16,16)
-blockStride = (8,8)
-cellSize = (8,8)
-nbins = 9
-derivAperture = 1
-winSigma = 4.
-histogramNormType = 0
-L2HysThreshold = 2.0000000000000001e-01
-gammaCorrection = 0
-nlevels = 64
+winSize = (64,64)#tamanho da janela
+blockSize = (16,16)#tamanho dos blocos/subimagens
+blockStride = (8,8)#distancia entre os blocos
+cellSize = (8,8)#tamanho da celula dos histogramas
+nbins = 9#valores do histograma
+derivAperture = 1#abertura do gradient
+winSigma = 4.#suavização gaussiana
+histogramNormType = 0#tipo de normalização usada
+L2HysThreshold = 2.0000000000000001e-01#limiar de normalização
+gammaCorrection = 0#Correção da gama da imagem
+nlevels = 64#quantidade de niveis usado para a orientação
 
 hog = cv.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
 
-#compute(img[, winStride[, padding[, locations]]]) -> descriptors
 
 winStride = (8,8)
 padding = (8,8)
 locations = ((10,20),)
 
 hist = hog.compute(image,winStride,padding,locations)
-hist=cv.normalize(hist, hist, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+#hist=cv.normalize(hist, hist, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
 
-hist2 = hog.compute(image2,winStride,padding,locations)
-hist2=cv.normalize(hist2, hist2, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+hist2 = hog.compute(image2,winStride,padding,locations) 
+#hist2=cv.normalize(hist2, hist2, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
 
 #print(hist)
 #print(hist2)
